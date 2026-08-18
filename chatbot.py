8c3a257967a78717a2d1cd813fdc7b3eb2c57155
omamaniaz <omamaniaz10@gmail.com>
2026-08-18 13:29:24 +0500

    Implement simple chatbot with basic responses

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+def get_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hi!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+    elif user_input == "bye":
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand that."
+
+
+print("Chatbot: Hello! I am a simple chatbot.")
+print("Chatbot: You can type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ")
+    reply = get_response(user_input)
+    print("Chatbot:", reply)
+
+    if user_input.lower() == "bye":
+        break
